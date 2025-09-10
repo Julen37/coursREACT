88e36834d965dc0c12cb6c0a6d0f6c0a504045b4
Julen37 <julen.gouchault@gmail.com>
2025-09-10 09:56:43 +0200

    if else

--- my-app/src/components/Cars.jsx
+++ my-app/src/components/Cars.jsx
@@ -5,14 +5,19 @@ const Cars = ({children, color}) => {
   // console.log(props.children);
 
 // condition if en ternaire
-  const colorInfo = color ? <p>Couleur : { color } </p> : <p>Couleur : Néant </p>;
+  // const colorInfo = color ? <p>Couleur : { color } </p> : <p>Couleur : Néant </p>;
+  // au lieu de mettre en deux ligne avec la const + le { colorInfo } on peut ecrire tout direct pour economiser une ligne
 
-  return (
-    <div style={ {backgroundColor: 'red', width: '400px', padding: '10px', margin:'5px auto'} }> 
-      <p>Marque : { children }</p>
-      { colorInfo }
-    </div>
-  )
+  if(children){
+    return (
+      <div style={ {backgroundColor: 'red', width: '400px', padding: '10px', margin:'5px auto'} }> 
+        <p>Marque : { children }</p>
+        { color ? <p>Couleur : { color } </p> : <p>Couleur : Néant </p> }
+      </div>
+    )
+  } else {
+    return <p>Pas de data!</p>
+  }
 }
 
 export default Cars
