ad4f628800c61f7fdf8dbd1d1ac6a1152722bff7
Julen37 <julen.gouchault@gmail.com>
2025-09-10 09:58:24 +0200

    ternaire dans un return

--- my-app/src/components/Cars.jsx
+++ my-app/src/components/Cars.jsx
@@ -8,16 +8,12 @@ const Cars = ({children, color}) => {
   // const colorInfo = color ? <p>Couleur : { color } </p> : <p>Couleur : Néant </p>;
   // au lieu de mettre en deux ligne avec la const + le { colorInfo } on peut ecrire tout direct pour economiser une ligne
 
-  if(children){
-    return (
-      <div style={ {backgroundColor: 'red', width: '400px', padding: '10px', margin:'5px auto'} }> 
-        <p>Marque : { children }</p>
-        { color ? <p>Couleur : { color } </p> : <p>Couleur : Néant </p> }
-      </div>
-    )
-  } else {
-    return <p>Pas de data!</p>
-  }
+  return children ? (
+    <div style={ {backgroundColor: 'red', width: '400px', padding: '10px', margin:'5px auto'} }> 
+      <p>Marque : { children }</p>
+      { color ? <p>Couleur : { color } </p> : <p>Couleur : Néant </p> }
+    </div>
+  ) : <p>Pas de data!</p>
 }
 
 export default Cars
