e50913f6643c886c10938b17f624ebc4846b4a15
Julen37 <julen.gouchault@gmail.com>
2025-09-10 09:36:07 +0200

    destructuring

--- my-app/src/components/Cars.jsx
+++ my-app/src/components/Cars.jsx
@@ -1,13 +1,13 @@
 // rafce pour creer une arrow function
 
-const Cars = (props) => {
+const Cars = ({children, color}) => {
 
   // console.log(props.children);
 
   return (
     <div style={ {backgroundColor: 'red', width: '400px', padding: '10px', margin:'5px auto'} }> 
-      <p>Marque : { props.children }</p>
-      <p>Couleur : { props.color } </p>
+      <p>Marque : { children }</p>
+      <p>Couleur : { color } </p>
     </div>
   )
 }
