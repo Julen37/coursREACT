a5d760e201484aca53646f047c248a2de39321c5
Julen37 <julen.gouchault@gmail.com>
2025-09-11 15:56:11 +0200

    exo 4 un objet tableau dans le state

--- my-app/src/components/MyCars.jsx
+++ my-app/src/components/MyCars.jsx
@@ -3,14 +3,22 @@ import Car from './Cars'
 
 class Mycars extends Component {
 
+    state = {
+        voiture: [
+            {name: 'Ford', year: '2000', color: 'red'},
+            {name: 'Mercedes', year: '2001', color: 'blue'},
+            {name: 'Peugeot', year: '2020', color: ''}
+        ]
+    }
+
     render() {
         return (
             <div>
                 <h1>{this.props.title}</h1>
                     
-                <Car year={"2000"} color="red">Ford</Car>
-                <Car>Mercedes</Car>
-                <Car color="green"></Car>
+                <Car color={this.state.voiture[0].color} year={this.state.voiture[0].year}>{this.state.voiture[0].name}</Car>
+                <Car color={this.state.voiture[1].color} year={this.state.voiture[1].year}>{this.state.voiture[1].name}</Car>
+                <Car color={this.state.voiture[2].color} year={this.state.voiture[2].year}>{this.state.voiture[2].name}</Car>
             </div>
            
         )
