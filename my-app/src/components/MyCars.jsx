a568e63829f03811a2f3fb33a614b232cac20ca6
Julen37 <julen.gouchault@gmail.com>
2025-09-11 16:09:47 +0200

    addTenYears function / map

--- my-app/src/components/MyCars.jsx
+++ my-app/src/components/MyCars.jsx
@@ -11,11 +11,24 @@ class Mycars extends Component {
         ]
     }
 
+    addTenYears = () => {
+
+        const updatedState = this.state.voiture.map((param) => {
+            return param.year -= 10;
+        })
+
+        this.setState(
+            {updatedState}
+        )
+    }
+
     render() {
         return (
             <div>
                 <h1>{this.props.title}</h1>
                     
+                <button onClick={this.addTenYears}> + 10 ans </button>
+
                 <Car color={this.state.voiture[0].color} year={this.state.voiture[0].year}>{this.state.voiture[0].name}</Car>
                 <Car color={this.state.voiture[1].color} year={this.state.voiture[1].year}>{this.state.voiture[1].name}</Car>
                 <Car color={this.state.voiture[2].color} year={this.state.voiture[2].year}>{this.state.voiture[2].name}</Car>
